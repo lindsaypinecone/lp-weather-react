93223e57e0391fbbd9c9b9da7e37dbbfcfefc7e9
lindsaypinecone <75231000+lindsaypinecone@users.noreply.github.com>
2021-03-01 10:07:27 -0500

    added target to footer link

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,10 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <a href="https://github.com/lindsaypinecone/lp-weather-react">
+      <a
+        href="https://github.com/lindsaypinecone/lp-weather-react"
+        target="_blank"
+      >
         Open-Source Code{" "}
       </a>
       by Lindsay Perricone
